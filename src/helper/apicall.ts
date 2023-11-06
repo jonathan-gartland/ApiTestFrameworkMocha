aea8c543bcf7e42c88be450f6df8c745689d5989
Jonathan Gartland <jgartland@Jonathans-MacBook-Air.local>
2023-11-06 15:20:45 -0800

    some cleanup, mostly remove comments

--- src/helper/apicall.ts
+++ src/helper/apicall.ts
@@ -1,17 +1,3 @@
-// import { request } from "src/config/supertest";
-// import { endpoint } from "src/services/endpoints";
-// import { Response } from "supertest";
-//
-// export const postCall = async (service: endpoint, payload: object, headers?: object): Promise<Response> => {
-//   if (headers) return request.post(service).set(headers);
-//   return request.post(service).send(payload);
-// }
-//
-// export const getCall = async (service: endpoint, payload?: object, headers?: object): Promise<Response> => {
-//   if (payload && headers) return request.get(service).set(headers).send(payload);
-//   else if (payload) return request.get(service).send(payload);
-//   else return request.get(service);
-// }
 import { request } from "@Config/supertest";
 import { logResponseToReport } from "@Helper/logger";
 import { GetApiType, PostApiType } from "@Types/http";
