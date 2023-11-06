6ca070281939317f72a49766d4831d4e0e69cfc7
Jonathan Gartland <jgartland@Jonathans-MacBook-Air.local>
2023-11-06 10:27:48 -0800

    added user test detail, remove payload

--- src/test/users.spec.ts
+++ src/test/users.spec.ts
@@ -1,19 +1,17 @@
 import { expect } from 'chai';
 import { createUserPayload } from 'src/resources/payload';
+import userdata from "src/resources/testdata.json"
 import supertest from 'supertest';
 
 const request = supertest("https://reqres.in/api/");
 
 describe('Test ReqRes APIs', () => {
 
-  it('TC_001_should validate create users', async () => {
-    // const createUserPayload = {
-    //   "name": "Jonny",
-    //   "job": "Testing"
-    // }
-    const response = await request.post('users').send(createUserPayload);
-    expect(response.statusCode).to.equal(201);
-    console.log(JSON.stringify(response.body));
-  })
-
+  userdata.forEach((user: {name: string, job: string}) => {
+    it(`Should validate create user ${user.name}`, async () => {
+      const response = await request.post('users').send(user);
+      expect(response.statusCode).to.equal(201);
+      console.log(JSON.stringify(response.body));
+    });
+  });
 })
